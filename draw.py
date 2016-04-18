a5920d810bea49bc65386d489ef827bd792269ad
jassq <1252348575@qq.com>
2016-04-19 02:02:19 +0800

    add

--- draw.py
+++ draw.py
@@ -6,8 +6,11 @@ import json
 from datetime import datetime as dt, timedelta as td
 from processData import Database
 from model import *
+import matplotlib.pyplot as plt
 
 
+EURUSD=[]
+DATE=[]
 
 
 @asyncio.coroutine
@@ -15,6 +18,12 @@ def Draw(engine):
     with (yield from engine) as conn:        
         res = yield from conn.execute(xm.select())
         for row in res:
-            print(row.DATE, row.EURUSD)
+            DATE.append(row.DATE)
+            EURUSD.append(row.EURUSD)
+
+        fig=plt.figure()
+        plt.plot(DATE, EURUSD)
+        fig.autofmt_xdate()
+        plt.show()
       
     pass
